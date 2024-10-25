e77668ef77754f6bee7d82914d7d0032396d4a1e
Tymoteusz <tymoteusz.majewski1@gmail.com>
2024-10-25 16:55:45 +0000

    fixed struct names

--- models/organization.go
+++ models/organization.go
@@ -6,10 +6,10 @@ import (
 	"github.com/google/uuid"
 )
 
-type Organizations struct {
-	ID        uuid.UUID    `db:"id" json:"id"`
-	Name      string       `db:"name" json:"name"`
-	Type      Organization `db:"type" json:"type"`
-	CreatedAt time.Time    `db:"created_at" json:"created_at"`
-	UpdatedAt time.Time    `db:"updated_at" json:"updated_at"`
+type Organization struct {
+	ID        uuid.UUID        `db:"id" json:"id"`
+	Name      string           `db:"name" json:"name"`
+	Type      OrganizationEnum `db:"type" json:"type"`
+	CreatedAt time.Time        `db:"created_at" json:"created_at"`
+	UpdatedAt time.Time        `db:"updated_at" json:"updated_at"`
 }
