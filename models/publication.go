67bac30df791a96cc13936f11bfb55d773a98e26
Tymoteusz <tymoteusz.majewski1@gmail.com>
2024-10-27 09:33:27 +0000

    added derived structures for CRUD operations for all of the models

--- models/publication.go
+++ models/publication.go
@@ -17,3 +17,24 @@ type Publication struct {
 	CreatedAt       time.Time `db:"created_at" json:"created_at"`
 	UpdatedAt       time.Time `db:"updated_at" json:"updated_at"`
 }
+type CreatePublication struct {
+	Title           string    `db:"title" json:"title"`
+	Journal         string    `db:"journal" json:"journal"`
+	PublicationDate time.Time `db:"publication_date" json:"publication_date"`
+	CitationsCount  int       `db:"citations_count" json:"citations_count"`
+	ImpactFactor    float64   `db:"impact_factor" json:"impact_factor"`
+	ScientistID     uuid.UUID `db:"scientist_id" json:"scientist_id"`
+}
+type UpdatePublication struct {
+	ID              uuid.UUID `db:"id" json:"id"`
+	Title           string    `db:"title" json:"title"`
+	Journal         string    `db:"journal" json:"journal"`
+	PublicationDate time.Time `db:"publication_date" json:"publication_date"`
+	CitationsCount  int       `db:"citations_count" json:"citations_count"`
+	ImpactFactor    float64   `db:"impact_factor" json:"impact_factor"`
+	ScientistID     uuid.UUID `db:"scientist_id" json:"scientist_id"`
+	UpdatedAt       time.Time `db:"updated_at" json:"updated_at"`
+}
+type DeletePublication struct {
+	ID uuid.UUID `db:"id" json:"id"`
+}
--- models/scientist.go
+++ models/scientist.go
@@ -17,3 +17,24 @@ type Scientist struct {
 	CreatedAt     time.Time `db:"created_at" json:"created_at"`
 	UpdatedAt     time.Time `db:"updated_at" json:"updated_at"`
 }
+type CreateScientist struct {
+	FirstName     string `db:"first_name" json:"first_name"`
+	LastName      string `db:"last_name" json:"last_name"`
+	AcademicTitle string `db:"academic_title" json:"academic_title"`
+	ResearchArea  string `db:"research_area" json:"research_area"`
+	Email         string `db:"email" json:"email"`
+	ProfileUrl    string `db:"profile_url" json:"profile_url"`
+}
+type UpdateScientist struct {
+	ID            uuid.UUID `db:"id" json:"id"`
+	FirstName     string    `db:"first_name" json:"first_name"`
+	LastName      string    `db:"last_name" json:"last_name"`
+	AcademicTitle string    `db:"academic_title" json:"academic_title"`
+	ResearchArea  string    `db:"research_area" json:"research_area"`
+	Email         string    `db:"email" json:"email"`
+	ProfileUrl    string    `db:"profile_url" json:"profile_url"`
+	UpdatedAt     time.Time `db:"updated_at" json:"updated_at"`
+}
+type DeleteScientist struct {
+	ID uuid.UUID `db:"id" json:"id"`
+}
