9d7d0b974b03a85eeb6bd156bf80094899e3e6ca
AdamN3wTS <adamjanus2004@gmail.com>
2024-10-29 20:04:45 +0100

    Add files via upload

---
+++ repositories/PublicationByID.go
@@ -0,0 +1,38 @@
+package models
+
+import (
+	"database/sql"
+	"io-project-api/models"
+
+	"github.com/google/uuid"
+)
+
+func PublicationByID(db *sql.DB, id uuid.UUID) ([]models.Publication, error) {
+	query := "SELECT id, title, journal, publication_date, citations_count, impact_factor, scientist_id, created_at,  updated_at FROM publications WHERE id =$1"
+	rows, err := db.Query(query, id)
+	if err != nil {
+		return nil, err
+	}
+
+	var Publications []models.Publication
+
+	for rows.Next() {
+		var Publication models.Publication
+		err := rows.Scan(
+			&Publication.ID,
+			&Publication.Title,
+			&Publication.Journal,
+			&Publication.PublicationDate,
+			&Publication.CitationsCount,
+			&Publication.ImpactFactor,
+			&Publication.ScientistID,
+			&Publication.CreatedAt,
+			&Publication.UpdatedAt,
+		)
+		if err != nil {
+			return nil, err
+		}
+		Publications = append(Publications, Publication)
+	}
+	return Publications, nil
+}
---
+++ repositories/ScientistOragnizationByID.go
@@ -0,0 +1,34 @@
+package models
+
+import (
+	"database/sql"
+	"io-project-api/models"
+
+	"github.com/google/uuid"
+)
+
+func ScientistOragnizationByID(db *sql.DB, id uuid.UUID) ([]models.ScientistOrganization, error) {
+	query := "SELECT id, scientist_id, organization_id FROM scientist_organization WHERE id = $1"
+	rows, err := db.Query(query, id)
+	if err != nil {
+		return nil, err
+	}
+
+	var scientistsOrganizations []models.ScientistOrganization
+
+	for rows.Next() {
+		var scientistOrganization models.ScientistOrganization
+		err := rows.Scan(
+			&scientistOrganization.ID,
+			&scientistOrganization.ScientistID,
+			&scientistOrganization.OrganizationID,
+			&scientistOrganization.CreatedAt,
+			&scientistOrganization.UpdatedAt,
+		)
+		if err != nil {
+			return nil, err
+		}
+		scientistsOrganizations = append(scientistsOrganizations, scientistOrganization)
+	}
+	return scientistsOrganizations, nil
+}
