56e8b0ed71d2358bd051a28a77db4dfa6b37d71b
Tymoteusz <tymoteusz.majewski1@gmail.com>
2024-11-06 09:41:03 +0000

    added ScientistPublicationService to retrieve scientist publication relationships by ID

---
+++ services/ScientistPublicationService.go
@@ -0,0 +1,28 @@
+package services
+
+import (
+	"database/sql"
+	"errors"
+	"io-project-api/models"
+	"io-project-api/repositories"
+
+	"github.com/google/uuid"
+	"go.uber.org/zap"
+)
+
+var (
+	ErrScientistPublicationNotFound = errors.New("scientist publication relationship not found for the given ID")
+)
+
+func GetScientistPublicationByID(db *sql.DB, id uuid.UUID) ([]models.ScientistPublication, error) {
+	scientistPublication, err := repositories.ScientistPublicationByID(db, id)
+	if err != nil {
+		zap.L().Error("Error querying scientist publication relationship by id", zap.Error(err))
+		return nil, err
+	}
+	if len(scientistPublication) == 0 {
+		zap.L().Error("No scientist publication relationship found", zap.String("ID", id.String()))
+		return nil, ErrScientistOrganizationNotFound
+	}
+	return scientistPublication, nil
+}
