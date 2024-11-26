8315d51a4e309a8036077d3d289bdb90b61be2fb
NavieteLogger <kacprzakmarek92@gmail.com>
2024-11-26 12:15:29 +0000

    Renamed services to follow the convention of ending with _services.go

---
+++ internal/services/search_services.go
@@ -0,0 +1,11 @@
+package services
+
+import "io-project-api/internal/models"
+
+func SearchForScientists() ([]models.Scientist, error) {
+	return nil, nil
+}
+
+func SearchForPublications() ([]models.Publication, error) {
+	return nil, nil
+}
