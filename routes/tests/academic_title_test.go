baf7e2dc398de52d8239d69d6c6834501877a7ba
Szymon <szymon.marciniak0@icloud.com>
2024-11-26 22:45:49 +0100

    Trying localhost  instead

--- routes/tests/academic_title_test.go
+++ routes/tests/academic_title_test.go
@@ -13,7 +13,7 @@ import (
 func TestRegisterAcademicTitle(t *testing.T) {
 
 	id := "e1e84e89-c064-4e42-887f-7c5aff43348d"
-	url := fmt.Sprintf("http://epickaporownywarkabazwiedzyuczelni.rocks:8000/docs#/api/scientists/%s", id)
+	url := fmt.Sprintf("http://localhost:8000/api/scientists/%s", id)
 
 	// Wykonaj zapytanie GET
 	resp, err := http.Get(url)
