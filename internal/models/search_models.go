9d687bfc50835bc44285ac2413e037f312f13e0a
NavieteLogger <kacprzakmarek92@gmail.com>
2024-12-02 15:47:31 +0000

    Added proper input fields for search query

--- internal/models/search_models.go
+++ internal/models/search_models.go
@@ -6,12 +6,39 @@ type SearchInput struct {
 }
 
 type SearchInputBody struct {
-	Titles             []AcademicTitle      `json:"titles" doc:"The academic titles to filer the search result."`
-	CitationsCounts    *CitationsCounts     `json:"citations_counts" doc:"The citation counts to filter the search result."`
-	MinisterialScores  *MinisterialScores   `json:"ministerial_scores" doc:"The ministerial scores to filter the search result."`
-	Organizations      []OrganizationsNames `json:"organizations" doc:"The organizations to filter the search result."`
-	PublicationsCounts *PublicationsCounts  `json:"publications_counts" doc:"The publications counts to filter the search result."`
-	ResearchAreas      []ResearchAreas      `json:"research_areas" doc:"The research areas to filter the search result."`
+	AcademicTitles     *TitlesRequest             `json:"academic_titles" doc:"The academic titles to filter the search result."`
+	CitationsCounts    *CitationsCountsRequest    `json:"citations_counts" doc:"The number of citations to filter the search result."`
+	MinisterialScores  *MinisterialScoresRequest  `json:"ministerial_scores" doc:"The ministerial scores to filter the search result."`
+	Organizations      *OrganizationsRequest      `json:"organizations" doc:"The organizations to filter the search result."`
+	PublicationsCounts *PublicationsCountsRequest `json:"publications_counts" doc:"The number of publications to filter the search result."`
+	ResearchAreas      *ResearchAreasRequest      `json:"research_areas" doc:"The research areas to filter the search result."`
+}
+
+type TitlesRequest struct {
+	Titles []AcademicTitle `json:"titles" doc:"The academic titles to filer the search result."`
+}
+
+type CitationsCountsRequest struct {
+	LowerBound int `json:"lower_bound" doc:"The minimum number of citations to filter the search result."`
+	UpperBound int `json:"upper_bound" doc:"The maximum number of citations to filter the search result."`
+}
+
+type MinisterialScoresRequest struct {
+	LowerBound int `json:"lower_bound" doc:"The minimum ministerial score to filter the search result."`
+	UpperBound int `json:"upper_bound" doc:"The maximum ministerial score to filter the search result."`
+}
+
+type OrganizationsRequest struct {
+	Organizations []OrganizationsNames `json:"organizations" doc:"The organizations to filter the search result."`
+}
+
+type PublicationsCountsRequest struct {
+	LowerBound int `json:"lower_bound" doc:"The minimum number of publications to filter the search result."`
+	UpperBound int `json:"upper_bound" doc:"The maximum number of publications to filter the search result."`
+}
+
+type ResearchAreasRequest struct {
+	ResearchAreas []ResearchAreas `json:"research_areas" doc:"The research areas to filter the search result."`
 }
 
 type CitationsCounts struct {
