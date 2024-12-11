2b206b5baa686a54d286d96627cbfbdfcdb04f42
Szymon <szymon.marciniak0@icloud.com>
2024-12-11 15:25:46 +0100

    added filters tests

---
+++ routes/tests/filters_test.go
@@ -0,0 +1,276 @@
+package tests
+
+import (
+	"encoding/json"
+	"fmt"
+	"io-project-api/internal/models"
+	_ "io-project-api/internal/responses"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"testing"
+)
+
+func TestGetAcademicTitleFilter(t *testing.T) {
+
+	url := "http://localhost:8000/api/filters/academic-titles"
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		t.Fatalf("Failed to create request: %v", err)
+	}
+
+	req.Header.Add("Accept", "application/json")
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		t.Fatalf("Failed to send request: %v", err)
+	}
+	defer res.Body.Close()
+
+	// Sprawdź, czy zapytanie zakończyło się sukcesem
+	if res.StatusCode != http.StatusOK {
+		log.Fatalf("Otrzymano błąd: %s", res.Status)
+	}
+
+	// Wczytaj odpowiedź
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Fatalf("Błąd podczas odczytywania odpowiedzi: %v", err)
+	}
+
+	// Rozpakuj JSON do struktury
+	var result []models.AcademicTitle
+
+	if err := json.Unmarshal(body, &result); err != nil {
+		log.Fatalf("Błąd podczas parsowania JSON: %v", err)
+	}
+
+	fmt.Printf("Academic titles: %v\n", result)
+}
+
+func TestGetMinisterialScoreFilter(t *testing.T) {
+	url := "http://localhost:8000/api/filters/ministerial-scores"
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		t.Fatalf("Failed to create request: %v", err)
+	}
+
+	req.Header.Add("Accept", "application/json")
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		t.Fatalf("Failed to send request: %v", err)
+	}
+	defer res.Body.Close()
+
+	// Sprawdź, czy zapytanie zakończyło się sukcesem
+	if res.StatusCode != http.StatusOK {
+		log.Fatalf("Otrzymano błąd: %s", res.Status)
+	}
+
+	// Wczytaj odpowiedź
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Fatalf("Błąd podczas odczytywania odpowiedzi: %v", err)
+	}
+
+	// Rozpakuj JSON do struktury
+	var result models.MinisterialScore
+	if err := json.Unmarshal(body, &result); err != nil {
+		log.Fatalf("Błąd podczas parsowania JSON: %v", err)
+	}
+
+	fmt.Printf("Smallest score: %v. Largest score:  %v.", result.Smallest, result.Largest)
+}
+func TestGetOrganizationsFilter(t *testing.T) {
+	url := "http://localhost:8000/api/filters/organizations"
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		t.Fatalf("Failed to create request: %v", err)
+	}
+
+	req.Header.Add("Accept", "application/json")
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		t.Fatalf("Failed to send request: %v", err)
+	}
+	defer res.Body.Close()
+
+	// Sprawdź, czy zapytanie zakończyło się sukcesem
+	if res.StatusCode != http.StatusOK {
+		log.Fatalf("Otrzymano błąd: %s", res.Status)
+	}
+
+	// Wczytaj odpowiedź
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Fatalf("Błąd podczas odczytywania odpowiedzi: %v", err)
+	}
+
+	// Rozpakuj JSON do struktury
+	var result []models.Organization
+	if err := json.Unmarshal(body, &result); err != nil {
+		log.Fatalf("Błąd podczas parsowania JSON: %v", err)
+	}
+	fmt.Printf("Organizations:")
+	for _, org := range result {
+		fmt.Printf(" %v\n", org)
+	}
+}
+
+func TestGetTraverseOrganizationsTree(t *testing.T) {
+
+	url := "http://localhost:8000/api/filters/organizations-tree"
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		t.Fatalf("Failed to create request: %v", err)
+	}
+
+	req.Header.Add("Accept", "application/json")
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		t.Fatalf("Failed to send request: %v", err)
+	}
+	defer res.Body.Close()
+
+	// Sprawdź, czy zapytanie zakończyło się sukcesem
+	if res.StatusCode != http.StatusOK {
+		log.Fatalf("Otrzymano błąd: %s", res.Status)
+	}
+
+	// Wczytaj odpowiedź
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Fatalf("Błąd podczas odczytywania odpowiedzi: %v", err)
+	}
+
+	// Rozpakuj JSON do struktury
+	var result []models.Organization
+	if err := json.Unmarshal(body, &result); err != nil {
+		log.Fatalf("Błąd podczas parsowania JSON: %v", err)
+	}
+	fmt.Printf("Organizations:")
+	for _, org := range result {
+		fmt.Printf(" %v\n", org)
+	}
+}
+
+func TestGetTraverseOrganizationsTreeByID(t *testing.T) {
+
+	id := "271e4cc1-4190-473c-98e0-65c316daf6ef"
+	url := fmt.Sprintf("http://localhost:8000/api/filters/organizations-tree?id=%s", id)
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		t.Fatalf("Failed to create request: %v", err)
+	}
+
+	req.Header.Add("Accept", "application/json")
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		t.Fatalf("Failed to send request: %v", err)
+	}
+	defer res.Body.Close()
+
+	// Sprawdź, czy zapytanie zakończyło się sukcesem
+	if res.StatusCode != http.StatusOK {
+		log.Fatalf("Otrzymano błąd: %s", res.Status)
+	}
+
+	// Wczytaj odpowiedź
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Fatalf("Błąd podczas odczytywania odpowiedzi: %v", err)
+	}
+
+	// Rozpakuj JSON do struktury
+	var result []models.Organization
+	if err := json.Unmarshal(body, &result); err != nil {
+		log.Fatalf("Błąd podczas parsowania JSON: %v", err)
+	}
+	fmt.Printf("Organizations:")
+	for _, org := range result {
+		fmt.Printf(" %v\n", org)
+	}
+}
+
+func TestPublicationsCountFilter(t *testing.T) {
+	url := "http://localhost:8000/api/filters/publication-counts"
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		t.Fatalf("Failed to create request: %v", err)
+	}
+
+	req.Header.Add("Accept", "application/json")
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		t.Fatalf("Failed to send request: %v", err)
+	}
+	defer res.Body.Close()
+
+	// Sprawdź, czy zapytanie zakończyło się sukcesem
+	if res.StatusCode != http.StatusOK {
+		log.Fatalf("Otrzymano błąd: %s", res.Status)
+	}
+
+	// Wczytaj odpowiedź
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Fatalf("Błąd podczas odczytywania odpowiedzi: %v", err)
+	}
+
+	// Rozpakuj JSON do struktury
+	var result models.PublicationCount
+	if err := json.Unmarshal(body, &result); err != nil {
+		log.Fatalf("Błąd podczas parsowania JSON: %v", err)
+	}
+
+	fmt.Printf("Publications largest count: %v. Publications smallest count: %v.", result.Largest, result.Smallest)
+}
+
+func TestGetResarchAreaFilter(t *testing.T) {
+	url := "http://localhost:8000/api/research-areas"
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		t.Fatalf("Failed to create request: %v", err)
+	}
+
+	req.Header.Add("Accept", "application/json")
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		t.Fatalf("Failed to send request: %v", err)
+	}
+	defer res.Body.Close()
+
+	// Sprawdź, czy zapytanie zakończyło się sukcesem
+	if res.StatusCode != http.StatusOK {
+		log.Fatalf("Otrzymano błąd: %s", res.Status)
+	}
+
+	// Wczytaj odpowiedź
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Fatalf("Błąd podczas odczytywania odpowiedzi: %v", err)
+	}
+
+	// Rozpakuj JSON do struktury
+	var result []models.ResearchArea
+	if err := json.Unmarshal(body, &result); err != nil {
+		log.Fatalf("Błąd podczas parsowania JSON: %v", err)
+	}
+	fmt.Printf("Resarch areas:")
+	for _, area := range result {
+		fmt.Printf(" %v\n", area)
+	}
+}
